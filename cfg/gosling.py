f58cd2fcc3ff279a4f27be66dea044a9f84eb497
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-02-22 22:37:15 -0500

    clean up logic and flow

--- cfg/gosling.py
+++ cfg/gosling.py
@@ -2,19 +2,30 @@ import json
 import csv
 
 def get_rules(node, parent_key, rules, grandparent_key="root"):
+    """
+    Traverse a JSON object and drill down recursively. Generate a set of CFG production rules.
+    Level: Spec and inner spec.
+    
+    Input: A node (a part of the JSON structure), a parent key (LHS), a rules list to store results, a grandparent key.
+           Grandparent key is to differentiate when parents are similar.
+           
+    Output: Append CFG rules to the rules list, which describe how the input structure maps to a set of properties.
+    """
     ARRAY_PROPERTIES = [
         'assembly', 'attributesToFields', 'categories', 'customFields', 'dashed', 'domain[color]', 
         'domain[row]', 'domain[stroke]', 'domain[y]', 'exonIntervalFields', 'fields', 'genomicFields', 
         'genomicFieldsToConvert', 'headerNames', 'inRange', 'interval', 'oneOf', 'range[color]', 
         'range[geneLabelColor]', 'range[size]', 'range[strandColor]', 'range[stroke]', 'range[y]', 
-        'replace', 'responsiveSpec', 'tooltip', 'valueFields', 'values', 'visibility', 'zoomLimits']
+        'replace', 'responsiveSpec', 'tooltip', 'valueFields', 'values', 'visibility', 'zoomLimits'
+    ]
     
     NUMBER_PROPERTIES = [
         'backgroundOpacity', 'binSize', 'centerRadius', 'dx', 'dy', 'height', 'maxInsertSize', 'maxRows', 
         'opacity', 'outlineWidth', 'padding', 'sampleLength', 'size', 'spacing', 'strokeWidth', 'textFontSize', 
         'textStrokeWidth', 'value[flag]', 'value[geneHeight]', 'value[geneLabelFontSize]', 
         'value[geneLabelOpacity]', 'value[geneLabelStrokeThickness]', 'value[opacity]', 'value[size]', 
-        'value[strokeWidth]', 'value[y]', 'width', 'xOffset', 'yOffset']
+        'value[strokeWidth]', 'value[y]', 'width', 'xOffset', 'yOffset'
+    ]
 
     STRING_PROPERTIES = [
         'background', 'baseGenomicField', 'baseline', 'chromosome', 'chromosomeField', 'chromosomePrefix', 
@@ -22,13 +33,14 @@ def get_rules(node, parent_key, rules, grandparent_key="root"):
         'groupMarksByField', 'id', 'include', 'indexUrl', 'legendTitle', 'linkingId', 'longToWideId', 
         'newField', 'outline', 'range[color]', 'row', 'separator', 'start', 'startField', 'stroke', 
         'subtitle', 'template', 'title', 'url', 'value[color]', 'value[data]', 'value[flag]', 
-        'value[geneLabelStroke]', 'value[stainStroke]', 'value[stroke]', 'value[text]']
+        'value[geneLabelStroke]', 'value[stainStroke]', 'value[stroke]', 'value[text]'
+    ]
 
     KEYS_WITH_GRANDPARENTS = ["domain", "range", "type", "value"]
     
-    # Adjust the key if it belongs to "KEYS_WITH_GRANDPARENTS"
+    # Adjust the key if it belongs to "KEYS_WITH_GRANDPARENTS": same key (parent), different grandparents
     adjusted_key = f"{parent_key}[{grandparent_key}]" if parent_key in KEYS_WITH_GRANDPARENTS else parent_key # LHS
-    rhs = ' "+" '.join(sorted(node.keys()))  # Construct RHS
+    rhs = ' "+" '.join(sorted(node.keys()))  # RHS
 
     this_rule = adjusted_key + ' -> ' + rhs
     rules.append(this_rule)
@@ -46,7 +58,7 @@ def get_rules(node, parent_key, rules, grandparent_key="root"):
             # Handle list (JS array)
             if new_parent in ARRAY_PROPERTIES:
                 rules.append(new_parent + ' -> ARRAY')  
-            elif k in ["tracks", "views"]: # Special handling for tracks, views, dataTransform
+            elif k in ["tracks", "views", "dataTransforms"]:                   # Special handling for tracks, views, dataTransform
                 for index, item in enumerate(v):
                     if isinstance(item, dict):               # Ensure it's a dictionary before recursion
                         get_rules(item, k[:-1], rules, parent_key)             # Convert "tracks" -> "track" etc.
@@ -56,7 +68,9 @@ def get_rules(node, parent_key, rules, grandparent_key="root"):
                 rules.append(new_parent + ' -> ' + str(v))   # Keep other lists as is
         else:
             # Handle terminal rules
-            if new_parent in NUMBER_PROPERTIES and isinstance(v, (int, float)):
+            if isinstance(v, bool):                          # Check boolean first, since True is treated as 1 in arithmetic operations
+                rules.append(new_parent + ' -> ' + '"' + str(v) + '"')  # keep as is
+            elif new_parent in NUMBER_PROPERTIES and isinstance(v, (int, float)):
                 rules.append(new_parent + ' -> NUMBER')
             elif new_parent in STRING_PROPERTIES and isinstance(v, str):
                 rules.append(new_parent + ' -> STRING')
@@ -64,6 +78,13 @@ def get_rules(node, parent_key, rules, grandparent_key="root"):
                 rules.append(new_parent + ' -> ' + '"' + str(v) + '"')
 
 def extract_rules(input_file, output_file, output_tsv):
+    """
+    Manage overall process: loading data, extracting rules using get_rules, and saving the rules to files.
+    Level: File.
+    
+    Input: Input TXT file with JSON data, and paths for output files.
+    Output: Extracted CFG rules to the specified output TXT and TSV files.
+    """
     specs = []
 
     # Read and parse JSON specs
