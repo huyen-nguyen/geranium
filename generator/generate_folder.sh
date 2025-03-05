34646e6ea7de6448ce6545e15a29f93b0d503116
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-03-05 16:58:00 -0500

    update gallery p2 dataset

--- generator/generate_folder.sh
+++ generator/generate_folder.sh
@@ -4,6 +4,6 @@ SCALE="0.7;1.0;1.2"
 SCALEWIDTH="0.7;1.0;1.2"
 
 # Iterate over all JSON files in the geranium_seeds directory
-for json_file in geranium_seeds/pv_cc/*.json; do
-    python generate_specs.py -f "$json_file" -pv -cc 
+for json_file in ./geranium_seeds/rerun/*.json; do
+    python generate_specs.py -f "$json_file" -pv -s $SCALE -sw $SCALEWIDTH
 done
