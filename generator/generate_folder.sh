10ab2744129cc15eeb95571e55e1e0bb4e510b8f
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-03-05 21:31:05 -0500

    done autogosling (only useful part)

--- generator/generate_folder.sh
+++ generator/generate_folder.sh
@@ -4,6 +4,6 @@ SCALE="0.7;1.0;1.2"
 SCALEWIDTH="0.7;1.0;1.2"
 
 # Iterate over all JSON files in the geranium_seeds directory
-for json_file in ./geranium_seeds/rerun/*.json; do
-    python generate_specs.py -f "$json_file" -pv -s $SCALE -sw $SCALEWIDTH
+for json_file in ./rerun_specs/*.json; do
+    python generate_specs.py -f "$json_file" -cc -s $SCALE -sw $SCALEWIDTH
 done
