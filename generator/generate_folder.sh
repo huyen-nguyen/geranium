e708a081ef5b26c9639e8890ff02dbd59ee077bb
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-03-12 22:17:26 -0400

    add comparative matrix examples, clean up

--- generator/generate_folder.sh
+++ generator/generate_folder.sh
@@ -5,5 +5,5 @@ SCALEWIDTH="0.7;1.0;1.2"
 
 # Iterate over all JSON files in the geranium_seeds directory
 for json_file in ./geranium_seeds/*.json; do
-    python generate_specs.py -f "$json_file" -cc -s $SCALE -sw $SCALEWIDTH
+    python generate_specs.py -f "$json_file" -pv -cc -s $SCALE -sw $SCALEWIDTH
 done
