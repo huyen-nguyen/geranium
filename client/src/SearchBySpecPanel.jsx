dfe47e095ee3eaa882faff33b9ec0791978d4544
sehilyi <sehilyi@gmail.com>
2025-03-19 14:42:49 -0400

    json file select and preview

--- client/src/SearchBySpecPanel.jsx
+++ client/src/SearchBySpecPanel.jsx
@@ -1,28 +1,38 @@
 import { useEffect, useMemo, useState } from 'react';
+import './SearchBySpecPanel.css';
 
 export default function SearchBySpecPanel(props) {
   /* eslint-disable react/prop-types */
   const { onUpdate } = props;
+  const [inputFile, setInputFile] = useState();
   const [inputSpec, setInputSpec] = useState();
 
   useEffect(() => {
-    if (inputSpec) {
-      // do something
+    if (inputFile) {
+      const reader = new FileReader();
+      reader.onload = e => { setInputSpec(e.target.result) }
+      reader.readAsText(inputFile);
     }
+  }, [onUpdate, inputFile]);
+
+  useEffect(() => {
+    onUpdate(inputSpec);
   }, [onUpdate, inputSpec]);
 
   const specPreview = useMemo(() => {
     if (inputSpec) {
-      return null
+      return (
+        <textarea className='spec-preview' value={inputSpec} readOnly />
+      );
     } else {
       return null;
     }
   }, [inputSpec]);
 
   return (
-    <div className='search-by-image-panel'>
-      <input type="file" accept="application/JSON" className='image-upload-button' onChange={(e) => { setInputSpec(e.currentTarget.files[0]) }} />
-      {inputSpec ? <h3>Preview</h3> : null}
+    <div className='search-by-spec-panel'>
+      <input type="file" accept="application/JSON" className='image-upload-button' onChange={(e) => { setInputFile(e.currentTarget.files[0]) }} />
+      {inputFile ? <h3>Preview</h3> : null}
       {specPreview}
     </div>
   );
