014b6f5ae28ef1c6942a9c916a0b0e80abad7cbc
SEHI L'YI <sehilyi@gmail.com>
2025-03-25 11:43:46 -0400

    Support text-based query (#19)
    * feat: first working demo with text-based query
    
    * chore: ignore __pycache__

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,58 @@
 import { useEffect, useMemo, useState } from 'react'
 import Header from './Header.jsx'
 import SearchPanel from './SearchPanel.jsx'
-// import axios from "axios"
+import axios from "axios"
+import './GalleryPanel.css'
+import './utils.js'
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 function App() {
 
-  const [searchParam, setSearchParam] = useState({});
+  const [topK, setTopK] = useState([]);
 
-  //const fetchAPI = async () => {
-  //  await axios.get("http://127.0.0.1:8080/api/users")
-  //}
+  const onSearch = async (type, text, image, spec) => {
+    const results = await axios.post('http://127.0.0.1:5000/api/get_inference', {
+      k: 10,
+      type,
+      content: type === 'Text' ? text : image
+    });
+    console.warn('New query results', results);
+    setTopK(results.data.data);
+  };
 
-  //useEffect(() => {
-  //  fetchAPI()
-  //}, [])
-
-  // Just for testing
-  const SearchedMsg = useMemo(() => {
+  const topKGallery = useMemo(() => {
     return (
-      <div className="search-result-panel">You clicked "Search." The input type is "{searchParam.type}" and the actual input is "{searchParam.text}" and "{searchParam.image + ''}" and "{searchParam.spec + ''}"</div>
-    )
-  }, [searchParam]);
+      <div className='gallery-panel'>
+        {topK.map(d => {
+          const title = d.name.split('_')[0].replaceAll('-', ' ').titleCase();
+          return (
+            <div className='gallery-item' key={d.name}>
+              <div className='gallery-item-title'>{`Example Name: ${d.name}`}</div>
+              <div className='gallery-image-text-group'>
+                <img className='gallery-item-thumbnail' src={`data:image/png;base64,${d.image}`} />
+                <div className='gallery-item-text'>
+                  <BsFillInfoCircleFill style={{
+                    color: '#4a4644',
+                    marginRight: '6px',
+                    verticalAlign: 'middle'
+                  }} />
+
+                  {d.text}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }, [topK]);
 
   return (
     <>
       <Header />
       <div className='content'>
-        <SearchPanel onSearch={(type, text, image, spec) => { setSearchParam({ type, text, image, spec }) }} />
-        {SearchedMsg}
+        <SearchPanel onSearch={onSearch} />
+        {topKGallery}
       </div>
     </>
   )
---
+++ client/src/utils.js
@@ -0,0 +1,7 @@
+String.prototype.titleCase = function() {
+  var words = this.valueOf().toLowerCase().split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+  }
+  return words.join(' ');
+}
