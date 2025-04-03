3eebd0be2d740cb2c773e0796363775503aa633a
thomcsmits <thomcsmits@outlook.com>
2025-04-03 11:53:22 -0400

    add modal view backbone for database landing page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+import { useMemo } from 'react';
+import axios from "axios"
 import './Header.css'
+import './Database.css'
 
 export default function Header() {
+
+  const [showDataBase, setShowDatabase] = useState();
+  const [indexData, setIndexData] = useState([]);
+
+  const onIndexData = async (type, Text, Image, Spec) => {
+    const results = await axios.get('http://127.0.0.1:5000/api/get_db');
+    console.warn('New query results', results);
+    setIndexData(results.data.data);
+  };
+
+  const dataBase = useMemo(() => {
+    if (showDataBase) {
+      return(
+        <div id='database' className='database-panel-dark-background' onClick={(e) => e.target.id === 'database' ? setShowDatabase(false) : null}>
+          <div className='database-panel'>
+            hello
+          </div>
+        </div>
+      )
+    }
+  }, [showDataBase, indexData])
+
   return (
     <div className="header">
       <p>Geranium</p>
-      <button>Gallery</button>
+      <button onClick={() => {
+        if (indexData.length === 0) {
+          onIndexData();
+        }
+        setShowDatabase(true);
+        }}>Gallery</button>
+      {dataBase}
     </div>
   )
 }
