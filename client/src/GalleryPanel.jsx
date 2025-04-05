7ae3abd63066e691dcf640a0a60541607fd9825f
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-04-05 01:33:22 -0400

    equal height when image height exceeds certain threshold

--- client/src/GalleryPanel.jsx
+++ client/src/GalleryPanel.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import './GalleryPanel.css'
+import { useEffect, useState } from 'react';
+import './GalleryPanel.css';
 import ExampleDetailView from './ExampleDetailView';
 import { LuExpand } from "react-icons/lu";
-import { prettierName } from './utils.js'
+import { prettierName } from './utils.js';
 import { IoCopy } from "react-icons/io5";
-
 
 export default function GalleryPanel(props) {
   /* eslint-disable react/prop-types */
@@ -13,76 +12,134 @@ export default function GalleryPanel(props) {
   const [copyNotification, setCopyNotification] = useState(false);
   const [itemCopyId, setItemCopyId] = useState(null);
 
+  useEffect(() => {
+    const adjustHeights = () => {
+      // Loop through each gallery item in the panel
+      const galleryItems = document.querySelectorAll('.gallery-item');
+      galleryItems.forEach(item => {
+        // Select the image, text, and spec elements.
+        // (Adjust the selector for the image if your actual class is different.)
+        const image = item.querySelector('.gallery-item-thumbnail');
+        const text = item.querySelector('.gallery-item-text');
+        const spec = item.querySelector('.gallery-item-spec');
+
+        if (image && text && spec) {
+          const imageHeight = image.clientHeight;
+          const textHeight = text.clientHeight;
+          const specHeight = spec.clientHeight;
+
+          // If the image is taller, update the height of text and spec to match
+          if (imageHeight !== textHeight) {
+            text.style.height = `${imageHeight}px`;
+          }
+          if (imageHeight !== specHeight) {
+            spec.style.height = `${imageHeight}px`;
+          }
+        }
+      });
+    };
+
+    // Run the adjustment after the component has rendered
+    adjustHeights();
+
+    // Listen for window resizes to re-run the height adjustment.
+    window.addEventListener('resize', adjustHeights);
+    return () => window.removeEventListener('resize', adjustHeights);
+  }, [data]); // re-run effect when data changes
+
   return (
-    <div className='gallery-panel'>
-      <ExampleDetailView selected={selected} setSelected={setSelected} copyNotification={copyNotification} setCopyNotification={setCopyNotification} />
-      {data.length > 0 ?
-      <>
-        <h2 style={{ marginTop: 0 }}>Search Results</h2>
-        {data.map((d, index) => {
-          const title = d.name.split('_')[0].replaceAll('-', ' ').titleCase();
-          return (
-            <div className='gallery-item' key={d.name}>
-              <div className='gallery-item-title' onClick={() => setSelected(d)}>{`Example Name: ${d.name}`}
-                <LuExpand style={{
-                  color: '#4a4644',
-                  verticalAlign: 'middle',
-                  marginLeft: '1rem'
-                }} />
-              </div>
-              <div className='gallery-image-text-group'>
-                <img className='gallery-item-thumbnail' onClick={() => setSelected(d)} src={`data:image/png;base64,${d.image}`} />
-                <div className='gallery-item-text'>
-                  {d.text}
-                </div>
-                <div className='gallery-item-spec'>
-                    <div className="textarea-with-copy">
-                  <textarea
-                      defaultValue={JSON.stringify(JSON.parse(d.spec), null, 2)}
-                      readOnly
-                  />
-                    <button
-                      className="textarea-copy-btn"
-                      onClick={() => {
-                        const specJson = JSON.stringify(JSON.parse(d.spec), null, 2);
-                        navigator.clipboard.writeText(specJson);
-                        setItemCopyId(index);
-                        setTimeout(() => setItemCopyId(null), 1500);
-                      }}
-                    >
-                      <IoCopy />
-                    </button>
-                    {itemCopyId === index && (
-                      <div className="textarea-copy-notification">
-                        Copied to clipboard!
+      <div className='gallery-panel'>
+        <ExampleDetailView
+            selected={selected}
+            setSelected={setSelected}
+            copyNotification={copyNotification}
+            setCopyNotification={setCopyNotification}
+        />
+        {data.length > 0 ? (
+            <>
+              <h2 style={{ marginTop: 0 }}>Search Results</h2>
+              {data.map((d, index) => {
+                const title = d.name.split('_')[0].replaceAll('-', ' ').titleCase();
+                return (
+                    <div className='gallery-item' key={d.name}>
+                      <div className='gallery-item-title' onClick={() => setSelected(d)}>
+                        {`Example Name: ${d.name}`}
+                        <LuExpand
+                            style={{
+                              color: '#4a4644',
+                              verticalAlign: 'middle',
+                              marginLeft: '1rem'
+                            }}
+                        />
                       </div>
-                    )}
-                  </div>
+                      <div className='gallery-image-text-group'>
+                        <img
+                            className='gallery-item-thumbnail'
+                            onClick={() => setSelected(d)}
+                            src={`data:image/png;base64,${d.image}`}
+                            alt={d.name}
+                        />
+                        <div className='gallery-item-text'>{d.text}</div>
+                        <div className='gallery-item-spec'>
+                          <div className="textarea-with-copy">
+                      <textarea
+                          defaultValue={JSON.stringify(JSON.parse(d.spec), null, 2)}
+                          readOnly
+                      />
+                            <button
+                                className="textarea-copy-btn"
+                                onClick={() => {
+                                  const specJson = JSON.stringify(JSON.parse(d.spec), null, 2);
+                                  navigator.clipboard.writeText(specJson);
+                                  setItemCopyId(index);
+                                  setTimeout(() => setItemCopyId(null), 1500);
+                                }}
+                            >
+                              <IoCopy />
+                            </button>
+                            {itemCopyId === index && (
+                                <div className="textarea-copy-notification">
+                                  Copied to clipboard!
+                                </div>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                );
+              })}
+            </>
+        ) : (
+            <>
+              <h2 style={{ marginTop: 0 }}>
+                Gallery Overview: Selected Examples
+                <span className="subtitle">Click to explore</span>
+              </h2>
+              <div className='landing-page-database'>
+                <div className='database-grid'>
+                  {databaseGallery.length > 0 ? (
+                      databaseGallery.map(d => {
+                        const title = prettierName(d.name);
+                        return (
+                            <div
+                                className='database-item'
+                                key={d.name}
+                                onClick={() => setSelected(d)}
+                                onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && setSelected(d)}
+                                tabIndex={0}
+                            >
+                              <div className='database-item-title'>{title}</div>
+                              <img className='database-item-thumbnail' src={`data:image/png;base64,${d.image}`} alt={d.name} />
+                            </div>
+                        );
+                      })
+                  ) : (
+                      <p>There are no examples to show.</p>
+                  )}
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </> :
-      <>
-        <h2 style={{ marginTop: 0 }}>Gallery Overview: Selected Examples<span className="subtitle">Click to explore</span></h2>
-        <div className='landing-page-database'>
-          <div className='database-grid'>
-            {databaseGallery.length > 0 ?
-            (databaseGallery.map(d => {
-              const title = prettierName(d.name);
-              return (
-                  <div className='database-item' key={d.name} onClick={() => setSelected(d)} onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && setSelected(d)} tabIndex={0}>
-                  <div className='database-item-title'>{title}
-                  </div>
-                  <img className='database-item-thumbnail' src={`data:image/png;base64,${d.image}`} />
-                </div>
-              )
-            })) : <p>There are no examples to show.</p>}
-          </div>
-        </div>
-      </>
-    }
-    </div>
+            </>
+        )}
+      </div>
   );
 }
