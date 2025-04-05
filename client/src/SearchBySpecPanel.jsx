da4de84d5faa6a45bed7575489c7cae46f92a08c
Huyen Nguyen <huyen26nguyen@gmail.com>
2025-04-05 02:00:33 -0400

    use monaco editor for spec search

--- client/src/SearchBySpecPanel.jsx
+++ client/src/SearchBySpecPanel.jsx
@@ -1,19 +1,18 @@
 import { useEffect, useMemo, useState } from 'react';
+import Editor from '@monaco-editor/react';
 import './SearchBySpecPanel.css';
 
 export default function SearchBySpecPanel(props) {
-  /* eslint-disable react/prop-types */
   const { onUpdate } = props;
   const [inputFile, setInputFile] = useState();
-  const [inputSpec, setInputSpec] = useState();
+  const [inputSpec, setInputSpec] = useState('');
 
   useEffect(() => {
     if (inputFile) {
       const reader = new FileReader();
       reader.onload = e => { setInputSpec(e.target.result) }
       reader.readAsText(inputFile);
-    }
-  }, [onUpdate, inputFile]);
+    }}, [onUpdate, inputFile]);
 
   useEffect(() => {
     onUpdate(inputSpec);
@@ -21,15 +20,37 @@ export default function SearchBySpecPanel(props) {
 
   const specPreview = useMemo(() => {
     return (
-      <textarea className='spec-preview' value={inputSpec ?? ''} onChange={(e) => setInputSpec(e.target.value)} />
+        <Editor
+            height="500px"
+            language="json"
+            value={inputSpec}
+            onChange={(value) => setInputSpec(value)}
+            theme="light"
+            options={{
+              minimap: { enabled: true },
+              tabSize: 2,
+              insertSpaces: true,          // Uses spaces for indentation.
+              detectIndentation: false,
+              fontSize: 14,
+            }}
+        />
     );
   }, [inputSpec]);
 
   return (
-    <div className='search-by-spec-panel'>
-      <input type="file" accept="application/JSON" className='image-upload-button' onChange={(e) => { setInputFile(e.currentTarget.files[0]) }} />
-      {inputFile ? <h3>Preview</h3> : null}
-      {specPreview}
-    </div>
+      <div className='search-by-spec-panel'>
+        <input
+            type="file"
+            accept="application/JSON"
+            className='image-upload-button'
+            onChange={(e) => {
+              setInputFile(e.currentTarget.files[0]);
+            }}
+        />
+        {inputFile ? <h3>Preview</h3> : null}<br/><br/>
+        <div className='editor-wrapper'>
+          {specPreview}
+        </div>
+      </div>
   );
 }
